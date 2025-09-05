b9f99b0462d64b1afcc3032526e47d7b654f242d
Vik Bhullar <epic_indus_dev@gmx.com>
2025-09-05 15:47:01 +0530

    Doubly linked list class def and methods for api

---
+++ doubly_linked_list/src/DoublyLinkedList.java
@@ -0,0 +1,34 @@
+package doubly_linked_list.src;
+
+public class DoublyLinkedList<T> {
+    Node<T> first = null;
+    Node<T> last = null;
+    int ItemsCount = 0;
+
+
+    private boolean isEmpty(){}
+
+    public int size(){}
+
+
+    public void insertAtBeginning(T item){}
+
+    public void insertAtEnd(T item){}
+
+    public void insertAfter(T item, Node<T> node){}
+
+    public void insertBefore(T item, Node<T> node){}
+
+    public T removeFromBeginning(){}
+
+    public T removeFromEnd(){}
+
+    public T deleteNode(Node<T> node){}
+
+    public T deleteValue(T item){}
+
+    public search(T item){}
+
+
+    
+}
