ecd8f972800cda07f97b1d39287ce3133f2d5837
Vik Bhullar <epic_indus_dev@gmx.com>
2025-09-05 15:49:46 +0530

    checks if list is empty

--- doubly_linked_list/src/DoublyLinkedList.java
+++ doubly_linked_list/src/DoublyLinkedList.java
@@ -3,10 +3,12 @@ package doubly_linked_list.src;
 public class DoublyLinkedList<T> {
     Node<T> first = null;
     Node<T> last = null;
-    int ItemsCount = 0;
+    int itemsCount = 0;
 
 
-    private boolean isEmpty(){}
+    private boolean isEmpty(){
+        return itemsCount == 0;
+    }
 
     public int size(){}
 
@@ -28,6 +30,7 @@ public class DoublyLinkedList<T> {
     public T deleteValue(T item){}
 
     public search(T item){}
+
 
 
     
